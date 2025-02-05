a7afbe20e2d69a957ef525c2a29fd2cf01232051
Ecomtor2604 <ecomtor2604@g.educaand.es>
2025-02-05 12:30:04 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
-basic.showIcon(IconNames.Heart)
+Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Red)
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
+    music.play(music.stringPlayable("C5 A B B A G E A ", 120), music.PlaybackMode.UntilDone)
+})
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == maqueen.readPatrol(maqueen.Patrol.PatrolRight)) {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 80)
+        maqueen.motorStop(maqueen.Motors.M1)
+        basic.pause(500)
+    }
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-    basic.pause(1000)
-    maqueen.motorStop(maqueen.Motors.All)
-    basic.pause(1000)
 })
